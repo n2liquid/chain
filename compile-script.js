5a957b8b3675baae7c445f5c21cb55f12dca2a24
Guilherme Vieira <super.driver.512@gmail.com>
2015-06-21 18:03:40 -0300

    refactor compile-script: Replace switch statement with hashmap of functions.

--- compile-script.js
+++ compile-script.js
@@ -5,6 +5,7 @@ let Q = require('q');
 let peg = require('pegjs');
 let errorWithMetadata = require('./util/error-with-metadata');
 let LowLevelScript = require('./low-level-script');
+let commandMappers = {};
 let parse = peg.buildParser (
 	fs.readFileSync (
 		__dirname + '/script-grammar.pegjs', {
@@ -22,32 +23,35 @@ module.exports = function(source) {
 		}
 		command.context = script;
 		let commandName = Object.keys(command)[0];
-		switch(commandName) {
-			case 'label':
-				script.labels[command.label] = i;
-				break;
-			case 'js': {
-				let fn = new Function(command.js.code);
-				let fnCommand = {
-					'@function': (function() {
-						fn.call(this);
-						return 'done';
-					})
-				};
-				delete command.js;
-				command = _.merge(fnCommand, command);
-				break;
-			}
-			case 'choice':
-				if(command.choice.objectLiteral) {
-					command.choice.code = command.choice.objectLiteral;
-				}
-				command.choice = new Function (
-					'return (' + command.choice.code + ');'
-				)();
-				break;
+		let commandMapper = commandMappers[commandName];
+		if(commandMapper) {
+			command = commandMapper(command, i);
 		}
 		return command;
 	});
 	return script;
+};
+commandMappers.label = function(command, commandOffset) {
+	command.context.labels[command.label] = commandOffset;
+	return command;
+};
+commandMappers.js = function(command) {
+	let fn = new Function(command.js.code);
+	let fnCommand = {
+		'@function': (function() {
+			fn.call(this);
+			return 'done';
+		}),
+	};
+	delete command.js;
+	return _.merge(fnCommand, command);
+};
+commandMappers.choice = function(command) {
+	if(command.choice.objectLiteral) {
+		command.choice.code = command.choice.objectLiteral;
+	}
+	command.choice = new Function (
+		'return (' + command.choice.code + ');'
+	)();
+	return command;
 };

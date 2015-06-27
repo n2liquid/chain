938bb186dec39f85ec0f2bdbc0dba8654ea39ae6
Guilherme Vieira <super.driver.512@gmail.com>
2015-06-27 02:09:04 -0300

    io: Emit stdin data events.

--- src/cli-read.js
+++ src/cli-read.js
@@ -1,15 +1,42 @@
 'use strict';
+let EventEmitter = require('events').EventEmitter;
+let emitter = new EventEmitter();
 let Q = require('q');
 let readline = require('readline');
-module.exports = function(question) {
+let reading = false;
+function resetStdin() {
+	process.stdin.setRawMode(true);
+	process.stdin.resume();
+	process.stdin.setEncoding('utf8');
+}
+resetStdin();
+exports = module.exports = function(question) {
 	let deferred = Q.defer();
 	let rli = readline.createInterface ({
 		input: process.stdin,
 		output: process.stdout,
 	});
+	reading = true;
 	rli.question(question, function(answer) {
 		rli.close();
+		reading = false;
+		resetStdin();
 		deferred.resolve(answer);
 	});
 	return deferred.promise;
 };
+exports.emitWhileReading = true;
+process.stdin.on('data', function(data) {
+	if(data === '\x03') {
+		process.stdout.write('\n');
+		process.exit();
+	}
+	if(reading && !exports.emitWhileReading) {
+		return;
+	}
+	data = data.replace('\x7f', '\b');
+	emitter.emit('data', data, reading);
+});
+['on', 'once', 'removeListener'].forEach(function(method) {
+	exports[method] = emitter[method].bind(emitter);
+});
